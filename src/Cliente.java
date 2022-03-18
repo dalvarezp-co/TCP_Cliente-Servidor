d116f4d45ac4712843a09c08a982fddff84728e9
Jairo Céspedes <ja.cespedes@uniandes.edu.co>
2022-03-18 18:04:20 -0500

    Yei

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,10 @@
 //Hola
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,6 +29,17 @@ public class Cliente {
  
             //Envio un mensaje al cliente
             out.writeUTF("¡Hola mundo desde el cliente!");
+            
+            byte[] contents = new byte[100000];
+            //Initialize the FileOutputStream to the output file's full path.
+            FileOutputStream fos = new FileOutputStream("D:\\Escritorio\100MB1.bin");
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            InputStream is = sc.getInputStream();
+            //No of bytes read in one read() call
+            int bytesRead = 0;
+            while((bytesRead=is.read(contents))!=-1)
+            bos.write(contents, 0, bytesRead);
+            bos.flush();
  
             //Recibo el mensaje del servidor
             String mensaje = in.readUTF();
